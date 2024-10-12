aa10f4d22d1cefc1c541307b550fef57e0069e2b
TymofiiPys <71066289+TymofiiPys@users.noreply.github.com>
2024-10-12 15:42:46 +0300

    Update BB API

--- src/main/java/org/example/websyslab1_proj/controller/Controller.java
+++ src/main/java/org/example/websyslab1_proj/controller/Controller.java
@@ -1,18 +1,35 @@
 package org.example.websyslab1_proj.controller;
 
+import org.example.websyslab1_proj.dto.BBQuoteDTO;
 import org.example.websyslab1_proj.entity.Resp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 @RestController
 public class Controller {
-
+    private final RestClient restClient;
 //    @GetMapping("/products/{id}")
     public Resp index(@PathVariable(name="id") int id) {
         Resp ret = new Resp();
         ret.id = id;
         ret.name = id + " name";
         return ret;
+    }
+
+    public Controller()
+    {
+        this.restClient = RestClient.create("https://api.breakingbadquotes.xyz/v1");
+    }
+    @GetMapping("/breakingbad/{number}")
+    public ResponseEntity<BBQuoteDTO[]> breakingBadNQuotes(@PathVariable(name = "number") int number) {
+        return ResponseEntity.ok(restClient.get().uri("/quotes/" + number).retrieve().body(BBQuoteDTO[].class));
+    }
+    @GetMapping("/breakingbad")
+    public ResponseEntity<BBQuoteDTO[]> breakingBadRandomQuote() {
+        return ResponseEntity.ok(restClient.get().uri("/quotes").retrieve().body(BBQuoteDTO[].class));
     }
 }
---
+++ src/main/java/org/example/websyslab1_proj/dto/BBQuoteDTO.java
@@ -0,0 +1,13 @@
+package org.example.websyslab1_proj.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BBQuoteDTO {
+    private String quote;
+    private String author;
+}
