9ebe16afc19cfb7aae2bd2a316fef633810a51f4
TymofiiPys <71066289+TymofiiPys@users.noreply.github.com>
2024-10-12 15:42:46 +0300

    Update BB API

--- src/main/java/org/example/websyslab1_proj/repository/ProductRepository.java
+++ src/main/java/org/example/websyslab1_proj/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package org.example.websyslab1_proj.repository;
 
 import org.example.websyslab1_proj.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
