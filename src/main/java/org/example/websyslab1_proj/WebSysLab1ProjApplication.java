70a6bcd098fe53a6331eb4cd6ca256ec6aeceb74
TymofiiPys <nfstimoha2004@gmail.com>
2024-11-12 21:13:59 +0200

    add cachemanager

--- src/main/java/org/example/websyslab1_proj/WebSysLab1ProjApplication.java
+++ src/main/java/org/example/websyslab1_proj/WebSysLab1ProjApplication.java
@@ -1,14 +1,23 @@
 package org.example.websyslab1_proj;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.example.websyslab1_proj.entity.Product;
 import org.example.websyslab1_proj.repository.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+@EnableCaching
 @SpringBootApplication
 public class WebSysLab1ProjApplication {
 
@@ -23,5 +32,17 @@ public class WebSysLab1ProjApplication {
 			Product product2 = new Product(2L, "second_prod");
 			productRepository.saveAll(List.of(product1, product2));
 		};
+	}
+
+	@Bean
+	public CacheManager cacheManager(){
+		SimpleCacheManager cacheManager = new SimpleCacheManager();
+		CaffeineCache bbCache = new CaffeineCache("breakingbadquote",
+				Caffeine.newBuilder()
+						.expireAfterWrite(1, TimeUnit.MINUTES)
+						.maximumSize(20)
+						.build());
+		cacheManager.setCaches(List.of(bbCache));
+		return cacheManager;
 	}
 }
